138ec69e65cdb5903656e9236275aa6abf354d0e
alek <wait82985@gmail.com>
2025-05-21 16:26:52 +0500

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,53 +1,53 @@
-public class Solution {
+class Solution {
     public void setZeroes(int[][] matrix) {
+        boolean firstRow = false;
+        boolean firstCol = false;
         int m = matrix.length;
         int n = matrix[0].length;
-        boolean isFirstColumnZeroed = false;
-        boolean isFirstRowZeroed = false;
-        
-        for (int i = 0; i < m; i++) {
-            if(matrix[i][0] == 0){
-                isFirstColumnZeroed = true;
-                break;
-            }
-            
-        }
 
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++){
             if(matrix[0][i] == 0){
-                isFirstRowZeroed = true;
-                break;
+                firstRow = true;
             }
         }
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
+        for(int i = 0; i < m; i++){
+            if(matrix[i][0] == 0){
+                firstCol = true;
+            }
+        }
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
                 if(matrix[i][j] == 0){
                     matrix[i][0] = 0;
                     matrix[0][j] = 0;
                 }
             }
         }
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+
+        for(int i = 1; i < m; i++){
+            if(matrix[i][0] == 0){
+                for(int j = 1; j < n; j++){
                     matrix[i][j] = 0;
                 }
             }
         }
-
-        if(isFirstColumnZeroed){
-            for (int i = 0; i < m; i++) {
+        for(int j = 1; j < n; j++){
+            if(matrix[0][j] == 0){
+                for(int i = 1; i < m; i++){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if(firstCol){
+            for(int i = 0; i < m; i++){
                 matrix[i][0] = 0;
             }
         }
-
-        if(isFirstRowZeroed){
-            for (int i = 0; i < n; i++) {
+        if(firstRow){
+            for(int i = 0; i < n; i++){
                 matrix[0][i] = 0;
             }
         }
 
-
-        
     }
 }
